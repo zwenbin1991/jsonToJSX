c47ef35923ecad2a0820bfa06e3cd86bb260e5f1
wjj <13524220689@189.cn>
2016-05-25 23:05:47 +0800

    json object => json tree object

--- src/lib/parseJSXTree.js
+++ src/lib/parseJSXTree.js
@@ -7,28 +7,44 @@
 
 'use strict';
 
-/*
+/**
 * json=>jsx tree object
 *
 * @param {Object|Array} jsons 数据
 * @param {String} componentName 组件名
-* @param {Array} childrens 子组件集合
+* @param {Array} childrens 子组件集合 [可选]
 *
-* example：
-*   var jsx = parseJSXTree([{ id: '1', nickname: 'aa', bg: 'url(../xx.jpg)', onClick: function () {} }, { id: '2', nickname: 'bb', bg: 'url(../oo.jpg)' }],
- *      'Page',
-  *     [ {
- *        componentName: 'div ,
-          props: {{ id: '1', nickname: 'aa', bg: 'url(../xx.jpg)', onClick: function () {} }
- *      } ])
- * export:
-      * {
-      *     componentName: 'Page',
-      *     props: {}
-      * }
-* */
-export const parseJSXTree = (jsons, componentName = 'div', childrens = []) => {
+example：
+  var jsx = parseJSXTree([{ id: 0, nickname: 'aa', bg: 'url(../xx.jpg)' }, { id: 1, nickname: 'bb', bg: 'url(../oo.jpg)' }],
+       'Page',
+       [
+           [
+               {
+                   componentName: 'div',
+                    props: { id: 0, idx: 0, onClick: function () {} }
+               },
+               {
+                   componentName: 'span',
+                    props: { id: 1, idx: 1, onClick: 'this.click' }
+               }
+        ]
+     ]);
+
+ print:
+     [
+          {
+               componentName: 'Page',
+               props: { id: 0, nickname: 'aa', bg: 'url(../xx.jpg)' },
+               childrens: [{ componentName: 'div', props: { id: 0, idx: 0, onClick: function () {} }, { componentName: 'span', props: { id: 1, idx: 1, onClick: 'this.click' } } }]
+          },
+          {
+               componentName: 'Page',
+               props: { id: 1, nickname: 'bb', bg: 'url(../oo.jpg)' }
+          }
+     ]
+  */
+export const parseJSXTree = (jsons, componentName = 'div', childrens) => {
     jsons = Array.isArray(jsons) ? jsons : [jsons];
 
-    return jsons.map(json => ({ componentName: componentName, props: json, childrens: childrens }));
+    return jsons.map((json, idx) => ({ componentName: componentName, props: json, childrens: childrens[idx] }));
 };
